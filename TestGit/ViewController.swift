9d93e4c552e057c07239e6a2613511f344e49a9a
Sajjan Giri <bsaiacademy@gmail.com>
2018-04-18 21:57:01 +0545

    print used in viewdidload

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("updated this view")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
